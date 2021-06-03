7924437e2fb0ab2ad9e0858faf43fff3c7aa4e75
Kyle <kyleyoung.9210@gmail.com>
2021-06-03 10:09:46 -0400

    initial commit. created react app. npm i bootstrap

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+	return (
+		<nav className="navbar navbar-dark bg-dark">
+			<div className="container-fluid d-flex justify-content-center">
+				<span className="navbar-brand mb-0 h1">Employee Directory</span>
+			</div>
+		</nav>
+	);
+};
+
+export default NavBar;
