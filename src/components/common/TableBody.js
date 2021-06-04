55b2542fb0fffa2aedaf23ae032e8d4e1e52f541
Kyle <kyleyoung.9210@gmail.com>
2021-06-04 08:48:29 -0400

    implementing sorting using lodash. passing through sortedList to table body to display sorted list

--- src/components/common/TableBody.js
+++ src/components/common/TableBody.js
@@ -3,20 +3,24 @@ import React, { Component } from 'react';
 class TableBody extends Component {
 	state = {};
 	render() {
-		const { result } = this.props;
+		const { sortedList } = this.props;
 		return (
 			<tbody>
-				{result.map((e) => (
-					<tr key={e.id.value}>
-						<td className="text-center">
-							<img src={e.picture.thumbnail} alt="" />
-						</td>
-						<td className="text-center">{`${e.name.first} ${e.name.last}`}</td>
-						<td className="text-center">{e.email}</td>
-						<td className="text-center">{`${e.location.city}, ${e.location.state}`}</td>
-						<td className="text-center">{e.cell}</td>
-					</tr>
-				))}
+				{sortedList.map((e) => {
+					let location = `${e.location.city}, ${e.location.state}`;
+					return (
+						<tr key={e.id.value}>
+							<td className="text-center">
+								<img src={e.picture.thumbnail} alt="" />
+							</td>
+							<td className="text-center">{e.name.first}</td>
+							<td className="text-center">{e.name.last}</td>
+							<td className="text-center">{e.email}</td>
+							<td className="text-center">{location}</td>
+							<td className="text-center">{e.cell}</td>
+						</tr>
+					);
+				})}
 			</tbody>
 		);
 	}
--- src/components/common/TableHeader.js
+++ src/components/common/TableHeader.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
 class TableHeader extends Component {
-	state = {};
+	sortColumn = (path) => {
+		const sortedColumn = { ...this.props.sortedColumn };
+		console.log('sortedColumn', sortedColumn);
+		console.log('sortedColumn.path', sortedColumn.path);
+		console.log('path', path);
+		if (sortedColumn.path === path && sortedColumn.path !== undefined) {
+			console.log('true');
+			sortedColumn.order = sortedColumn.order === 'asc' ? 'desc' : 'asc';
+		} else {
+			sortedColumn.path = path;
+			sortedColumn.order = 'asc';
+		}
+
+		this.props.onSort(sortedColumn);
+	};
+
 	render() {
 		const { columns } = this.props;
 		return (
 			<thead>
 				<tr>
+					<th className="text-center">Image</th>
 					{columns.map((column) => (
-						<th className="clickable text-center" key={column}>
-							{column}
+						<th
+							className="clickable text-center"
+							key={column.label}
+							onClick={() => this.sortColumn(column.path)}
+						>
+							{column.label}
 						</th>
 					))}
 				</tr>
