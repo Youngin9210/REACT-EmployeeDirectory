f8adda84e28be0322cd53ab66c3222ede6a5309c
Kyle <kyleyoung.9210@gmail.com>
2021-06-04 09:17:21 -0400

    changing font awesome icon classes to correct classes. up and down instead of asc and desc

--- src/components/common/TableHeader.js
+++ src/components/common/TableHeader.js
@@ -8,13 +8,22 @@ class TableHeader extends Component {
 		console.log('path', path);
 		if (sortedColumn.path === path && sortedColumn.path !== undefined) {
 			console.log('true');
-			sortedColumn.order = sortedColumn.order === 'asc' ? 'desc' : 'asc';
+			sortedColumn.order = sortedColumn.order === 'up' ? 'down' : 'up';
 		} else {
 			sortedColumn.path = path;
-			sortedColumn.order = 'asc';
+			sortedColumn.order = 'up';
 		}
 
 		this.props.onSort(sortedColumn);
+	};
+
+	renderSortIcon = (column) => {
+		const { sortedColumn } = this.props;
+		console.log('column.path', column.path);
+		console.log('sortedColumn.path', sortedColumn.path);
+		if (column.path !== sortedColumn.path) return null;
+		if (sortedColumn.order === 'up') return <i className="fa fa-sort-up"></i>;
+		return <i className="fa fa-sort-down"></i>;
 	};
 
 	render() {
@@ -29,7 +38,7 @@ class TableHeader extends Component {
 							key={column.label}
 							onClick={() => this.sortColumn(column.path)}
 						>
-							{column.label}
+							{column.label} {this.renderSortIcon(column)}
 						</th>
 					))}
 				</tr>
