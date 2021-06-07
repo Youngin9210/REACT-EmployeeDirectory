5274b82e6e36edc5b1aa947318574179484c0241
Kyle <kyleyoung.9210@gmail.com>
2021-06-07 12:42:56 -0400

    combining first and last name into one column. Moved text-center class to tr

--- src/components/common/TableBody.js
+++ src/components/common/TableBody.js
@@ -9,15 +9,16 @@ class TableBody extends Component {
 				{sortedList.map((e) => {
 					let location = `${e.location.city}, ${e.location.state}`;
 					return (
-						<tr key={e.id.value}>
-							<td className="text-center">
+						<tr key={e.id.value} className="text-center">
+							<td>
 								<img src={e.picture.thumbnail} alt="" />
 							</td>
-							<td className="text-center">{e.name.first}</td>
-							<td className="text-center">{e.name.last}</td>
-							<td className="text-center">{e.email}</td>
-							<td className="text-center">{location}</td>
-							<td className="text-center">{e.cell}</td>
+							<td>
+								{e.name.first} {e.name.last}
+							</td>
+							<td>{e.email}</td>
+							<td>{location}</td>
+							<td>{e.cell}</td>
 						</tr>
 					);
 				})}
