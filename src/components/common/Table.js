2636f8168e594a495b8216a2f60e4638c743ecb1
Kyle <kyleyoung.9210@gmail.com>
2021-06-07 12:39:42 -0400

    passing through getTableData prop

--- src/components/common/Table.js
+++ src/components/common/Table.js
@@ -2,13 +2,14 @@ import React from 'react';
 import TableBody from './TableBody';
 import TableHeader from './TableHeader';
 
-const Table = ({ columns, onSort, sortedColumn, sortedList }) => {
+const Table = ({ columns, onSort, sortedColumn, sortedList, getTableData }) => {
 	return (
 		<table className="table table-responsive">
 			<TableHeader
 				columns={columns}
 				onSort={onSort}
 				sortedColumn={sortedColumn}
+				getTableData={getTableData}
 			/>
 			<TableBody sortedList={sortedList} />
 		</table>
