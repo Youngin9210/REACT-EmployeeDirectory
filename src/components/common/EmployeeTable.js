db2676998318243ea10c9c65e7f3ce2bc8df5f20
Kyle <kyleyoung.9210@gmail.com>
2021-06-07 12:39:07 -0400

    removing last name column and passing through getTableData prop

--- src/components/common/EmployeeTable.js
+++ src/components/common/EmployeeTable.js
@@ -3,20 +3,20 @@ import Table from './Table';
 
 class EmployeeTable extends Component {
 	columns = [
-		{ path: 'name.first', label: 'First Name' },
-		{ path: 'name.last', label: 'Last Name' },
+		{ path: 'name.first', label: 'Name' },
 		{ path: 'email', label: 'Email' },
 		{ path: 'location.city', label: 'Location' },
 		{ path: 'cell', label: 'Cell' },
 	];
 	render() {
-		const { onSort, sortedColumn, sortedList } = this.props;
+		const { onSort, sortedColumn, sortedList, getTableData } = this.props;
 		return (
 			<Table
 				columns={this.columns}
 				onSort={onSort}
 				sortedColumn={sortedColumn}
 				sortedList={sortedList}
+				getTableData={getTableData}
 			/>
 		);
 	}
