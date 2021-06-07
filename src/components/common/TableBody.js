026b3b58be2a012bbd289a5e7c5b5bdfb19d0f29
Kyle <kyleyoung.9210@gmail.com>
2021-06-07 13:02:34 -0400

    importing ReactRoundedImage and rendering images as rounded images now with a border. Aligning rows middle

--- src/components/common/TableBody.js
+++ src/components/common/TableBody.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactRoundedImage from 'react-rounded-image';
 
 class TableBody extends Component {
 	state = {};
@@ -9,11 +10,17 @@ class TableBody extends Component {
 				{sortedList.map((e) => {
 					let location = `${e.location.city}, ${e.location.state}`;
 					return (
-						<tr key={e.id.value} className="text-center">
+						<tr key={e.id.value} className="text-center align-middle">
 							<td>
-								<img src={e.picture.thumbnail} alt="" />
+								<ReactRoundedImage
+									image={e.picture.large}
+									roundedSize="8"
+									roundedColor="#253d5b"
+									imageWidth="80"
+									imageHeight="80"
+								/>
 							</td>
-							<td>
+							<td className="">
 								{e.name.first} {e.name.last}
 							</td>
 							<td>{e.email}</td>
