d3998f71b6df6f356b126cfe2c1d01268d362842
Kyle <kyleyoung.9210@gmail.com>
2021-06-08 16:50:24 -0400

    increased font size of navbar brand 'Employee Directory'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
 	return (
 		<nav className="navbar navbar-dark p-3 employeeNav">
 			<div className="container-fluid d-flex justify-content-center">
-				<span className="navbar-brand mb-0 h1">Employee Directory</span>
+				<span className="navbar-brand fs-1 mb-0 h1">Employee Directory</span>
 			</div>
 		</nav>
 	);
